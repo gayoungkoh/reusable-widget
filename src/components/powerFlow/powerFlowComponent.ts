866cd44033079de11d71046153efe582fb321eea
gayoung.koh <gayoung.koh@qcells.com>
2025-04-17 22:16:34 +0900

    Renames PowerFlow component
    Updates the PowerFlow component name in the web component registration.
    
    This change ensures that the correct component is used when the
    web component is instantiated, resolving potential naming conflicts
    or incorrect component rendering.

--- src/components/powerFlow/powerFlowComponent.ts
+++ src/components/powerFlow/powerFlowComponent.ts
@@ -1,7 +1,7 @@
 import r2wc from "@r2wc/react-to-web-component";
-import { PowerFlow } from "@/components/powerFlow/PowerFlow";
+import { PowerFlowWrapper } from "@/components/powerFlow/PowerFlow";
 
-const powerFlowComponent = r2wc(PowerFlow, {
+const powerFlowComponent = r2wc(PowerFlowWrapper, {
   props: {
     siteId: "string",
   },
