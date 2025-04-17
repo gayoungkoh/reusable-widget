d9eb5bf6682f1b199bdbe241cdd6be675ed3bf98
gayoung.koh <gayoung.koh@qcells.com>
2025-04-17 15:38:46 +0900

    Opens shadow DOM for site overview chart
    Changes the shadow DOM mode of the site overview chart component from 'closed' to 'open'.
    This allows for easier styling and inspection of the component's internal structure.

--- src/components/siteOverviewChartComponent.ts
+++ src/components/siteOverviewChartComponent.ts
@@ -2,7 +2,7 @@ import r2wc from "@r2wc/react-to-web-component";
 import { SiteOverviewChartWrapper } from "@/components/SiteOverviewChart";
 
 const siteOverviewChartComponent = r2wc(SiteOverviewChartWrapper, {
-  shadow: "closed",
+  shadow: "open",
 });
 
 customElements.define("site-overview-chart", siteOverviewChartComponent);
