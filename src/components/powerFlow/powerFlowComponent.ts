f77a2fcd3602f2f41a2728e6d9d26aac528cb160
gayoung.koh <gayoung.koh@qcells.com>
2025-04-18 15:39:41 +0900

    Removes shadow DOM from web components
    Removes the shadow DOM option from the `power-flow` and `site-overview-chart` web components.
    This simplifies the components and allows for easier styling and integration with the host page.

--- src/components/powerFlow/powerFlowComponent.ts
+++ src/components/powerFlow/powerFlowComponent.ts
@@ -2,7 +2,6 @@ import r2wc from "@r2wc/react-to-web-component";
 import { PowerFlowWrapper } from "@/components/powerFlow/PowerFlow";
 
 const powerFlowComponent = r2wc(PowerFlowWrapper, {
-  shadow: "open",
   props: {
     "site-id": "string",
   },
--- src/components/siteOverviewChart/siteOverviewChartComponent.ts
+++ src/components/siteOverviewChart/siteOverviewChartComponent.ts
@@ -1,8 +1,6 @@
 import r2wc from "@r2wc/react-to-web-component";
 import { SiteOverviewChartWrapper } from "@/components/siteOverviewChart/SiteOverviewChart";
 
-const siteOverviewChartComponent = r2wc(SiteOverviewChartWrapper, {
-  shadow: "open",
-});
+const siteOverviewChartComponent = r2wc(SiteOverviewChartWrapper);
 
 customElements.define("site-overview-chart", siteOverviewChartComponent);
